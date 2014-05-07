cd538d989891a5f451d84c1a7046c7f608703482
John Hennelly <henns20@john-hennellys-macbook.local>
2014-05-07 16:06:08 -0400

    added an angular scaffolding yeoman, grunt, bower and npm

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('montyhallAngularGitApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Jamie'
+
+    ];
+  });
